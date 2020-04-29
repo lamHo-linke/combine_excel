872ced7b152532ced41f71c738b83f49a8aa5da6
lamHo <32195451+lamHo-linke@users.noreply.github.com>
2020-04-29 18:02:16 +0800

    Update combine_excel.py
    1. 修改输出表格，只输出合并的sheet，不合并的前几张表不输出。
    2. 添加从第几张sheet开始合并的选项，可满足更多的需求。
    3. 添加程序运行结束后不自动关闭窗口，可看到显示成功或报错信息。

--- combine_excel.py
+++ combine_excel.py
@@ -19,7 +19,8 @@ def get_path(path):
     file_path = []
     for path, dir_list, file_list in g:
         for file_name in file_list:
-            if re.search('xlsx$', file_name) != None:   # 只读xlsx文件地址
+            if (re.search('xlsx$', file_name) != None) and (re.search('~\$', file_name) == None):
+                # 只读xlsx文件地址，且不读取打开Excel时的临时文件
                 file_path.append(os.path.join(path, file_name))
     return file_path
 
@@ -54,45 +55,42 @@ def concat_one_sheet(excel,sheetname):
     
 
 
-def combine_sheets(excel):
+def combine_sheets(excel,s_start):
     """遍历所有文件合并对应sheet"""
-    """工作簿0~8不合并，取第一张表"""
+    """修改s_start值可以设置从哪张表开始进行合并"""
     
     sheets = sheets_name(excel)
+    s = sheets[s_start-1:]
     df = []
-    for s in range(len(sheets)):
-        if s<9:
-            df.append(excel[0][sheets[s]])    
-        else:
-            df.append(concat_one_sheet(excel, sheets[s]))    
+    for si in s:
+        df.append(concat_one_sheet(excel, si))    
     
     # 未提出部分工作簿不合并前，做的遍历所有sheet合并
     # for s in sheets:
     #     df.append(concat_one_sheet(excel,s))
-    return df
+    return df, s
 
 
 
-def save_file(output_path,excel):
+def save_file(output_path,df,sheets):
     """保存输出结果"""
     
-    sheets = sheets_name(excel)
     writer = pd.ExcelWriter(output_path)
-    df = combine_sheets(excel)
     for i,s in zip(range(len(sheets)),sheets):
         df[i].to_excel(writer, index=False,encoding='utf-8',header=None,sheet_name=s)
     writer.save()
-    print("合并结果已保存。（文件名为“合并结果+日期+时间”）")
+    print("恭喜合并完成！合并结果已保存在输出文件。（文件名为“合并结果+日期+时间”）")
     
 
 
 if __name__ == '__main__':
-    path = input("Hello! 晓兰~\n请确保文件夹下只有需要合并的Excel文件\n第一步：粘贴Excel文件所在的文件夹地址（不包括引号）：")
-    output = input("第二步：粘贴你想要输出文件保存的文件夹（不包括引号）：")
-    excel = read_excel(path)
+    path = input("Hello! 晓兰~\n请确保文件夹下只有需要合并的Excel文件\n第一步：粘贴Excel文件所在的文件夹地址：")
+    output = input("第二步：粘贴你想要输出文件保存的文件夹：")
+    start_sheet = int(input("第三步：选择从第几张子表开始实行合并（请填正整数）："))
+    
+    excel = read_excel(path)     # 从路径读取所有Excel文件
+    dataframe, sheet_name = combine_sheets(excel,s_start=start_sheet)    # 合成后生成的DataFrame
     time = time.strftime("%Y%m%d-%H%M%S", time.localtime())  
     output_path = output + r'\合并结果' + time + '.xlsx'     # 输出文件位置
-    save_file(output_path,excel)
-    
-    
-    
+    save_file(output_path,dataframe,sheet_name)
+    os.system("pause")    # 运行结束不关闭窗口
